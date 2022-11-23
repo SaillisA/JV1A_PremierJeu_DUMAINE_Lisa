7b00ab549e4cea7af54f64edfb3f7f763f42d762
SaillisA <dumaine.lisa04@gmail.com>
2022-11-23 17:05:23 +0100

    Javascript
    module attaque pour 1 seul joueur et riposte d'1 seul monstre finis :)

--- monScript.js
+++ monScript.js
@@ -1,18 +1,20 @@
-maDiv = document.getElementById("maDiv");
-pvMonstreActuel = document.getElementById("pvMonstreActuel");
-afficheActionMonstre = document.getElementById("afficheActionMonstre");
+pvMonstre1 = document.getElementById("pvMonstre1");
 
-pvJoueurActuel = document.getElementById("pvJoueurActuel");
-afficheActionJoueur = document.getElementById("afficheActionJoueur");
+pvJoueur1 = document.getElementById("pvJoueur1");
+afficheActions = document.getElementById("afficheActions");
 
-
+var cptTempsRiposte = 4
 
 
 AttaqueClick.onclick = function() {
-    pvMonstreActuel.innerHTML = parseInt(pvMonstreActuel.innerHTML)-10;
+    pvMonstre1.innerHTML = parseInt(pvMonstre1.innerHTML)-10;
     afficheActions.innerHTML = " Il a perdu 10 pv !";
+    cptTempsRiposte = cptTempsRiposte -1;
 
-    pvJoueurActuel.innerHTML = parseInt(pvJoueurActuel.innerHTML)-10;
-    afficheActions.innerHTML = " Vous avez perdu 10 pv !"
+    if(cptTempsRiposte==0){
+        pvJoueur1.innerHTML = parseInt(pvJoueur1.innerHTML)-10;
+        afficheActions.innerHTML = " Vous avez perdu 10 pv !";
+        cptTempsRiposte = 4;
+    }
 }
 
